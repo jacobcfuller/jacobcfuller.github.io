e1cd456f5bb020154d91f153318e9f269570702a
jcfuller <jacobcfuller93@gmail.com>
2017-04-16 20:26:33 -0400

    change index.html to real page

--- omdb/js/movie.js
+++ omdb/js/movie.js
@@ -74,7 +74,7 @@ function getMovie(){
             ${movie.Plot}
             <hr>
             <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+            <a href="omdb_search.html" class="btn btn-default">Go Back To Search</a>
           </div>
         </div>
       `;
